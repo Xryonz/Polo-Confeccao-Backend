c2c55ebc5f67b3b26b03cf12a8874b2d5af75bc3
Xryonz <i0560008@gmail.com>
2026-04-18 13:10:38 -0300

    debug env vars

--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,10 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+console.log('DB_HOST:', process.env.DB_HOST)
+console.log('DB_PORT:', process.env.DB_PORT)
+console.log('DB_NAME:', process.env.DB_NAME)
+
 const pool = mysql.createPool({
   host:     process.env.DB_HOST,
   user:     process.env.DB_USER,
